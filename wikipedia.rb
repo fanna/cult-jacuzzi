4e119921122da5d0f4226959c03f2b637f761b9b
Jovan Ivanovic <jovan.ivanovic3@gmail.com>
2016-11-12 00:26:10 +0100

    Wikipedia-based random paragraph generator

--- wikipedia.rb
+++ wikipedia.rb
@@ -2,15 +2,21 @@ require 'nokogiri'
 require 'open-uri'
 
 class Wikipedia
-  def self.random_article
+  def self.random_text
+    retries ||= 0
+
     url = "https://en.wikipedia.org/wiki/Special:Random"
     html = Nokogiri::HTML(open(url))
-    text = html.css("p").map { |x| x }.join("\n")
 
-    puts "---"
-    puts text
-    puts "---"
+    paragraphs = html.css("p").map { |x| x }.join("\n")
+    result = paragraphs.gsub!(/\[.*\]/, "").split(/\n/).sample
 
-    text.gsub!(/\[.*\]/, "").split(/[\n,\.]/)
+    raise unless result
+    raise if result == ""
+    raise if result.length < 150
+
+    result
+  rescue
+    retry if (retries += 1) < 10
   end
 end
