7c01073118e5297968903e66917517e9958d7a5e
Stefan Mijucic <fanna993@gmail.com>
2016-11-12 19:22:24 +0100

    Add collision

--- lib/player.rb
+++ lib/player.rb
@@ -22,6 +22,11 @@ class Player
     @cur_image.draw(@x + offs_x, @y - 49, 0, factor, 1.0)
   end
 
+  def collide(offs_x, offs_y)
+    not @map.solid?(@x + offs_x, @y + offs_y) and
+      not @map.solid?(@x + offs_x, @y + offs_y - 45)
+  end
+
   def update(move_x, move_y)
     if (move_x == 0 && move_y == 0)
       @cur_image = @standing
@@ -31,20 +36,20 @@ class Player
 
     if move_x > 0 then
       @dir = :right
-      move_x.times { @x += 1 }
+      move_x.times { if collide(1, 0) then @x += 1 end }
     end
     if move_x < 0 then
       @dir = :left
-      (-move_x).times { @x -= 1 }
+      (-move_x).times { if collide(-1, 0) then @x -= 1 end }
     end
 
     if move_y > 0 then
       @dir = :right
-      move_y.times { @y += 1 }
+      move_y.times { if collide(0, 1) then @y += 1 else move_y = 0 end }
     end
     if move_y < 0 then
       @dir = :left
-      (-move_y).times { @y -= 1 }
+      (-move_y).times { if collide(0, -1) then @y -= 1 else move_y = 0 end }
     end
   end
 end
