a335d64358a3978a5dbfd20efc3faf8af96d4ccc
Stefan Mijucic <fanna993@gmail.com>
2016-11-12 18:49:52 +0100

    Add player animation

--- lib/player.rb
+++ lib/player.rb
@@ -4,26 +4,46 @@ class Player
   def initialize(x, y, map)
     @x = x
     @y = y
+    @dir = :left
     @map = map
-    @player = Gosu::Image.new("./assets/horse.png")
+    @standing, @walk_left_right1, @walk_left_right2 = *Gosu::Image.load_tiles("./assets/test_player.png", 50, 50)
+
+    @cur_image = @standing
   end
 
   def draw
-    @player.draw(@x, @y, 0)
+    if @dir == :left then
+      offs_x = -25
+      factor = 1.0
+    else
+      offs_x = 25
+      factor = -1.0
+    end
+    @cur_image.draw(@x + offs_x, @y - 49, 0, factor, 1.0)
   end
 
   def update(move_x, move_y)
+    if (move_x == 0 && move_y == 0)
+      @cur_image = @standing
+    else
+      @cur_image = (Gosu::milliseconds / 175 % 2 == 0) ? @walk_left_right1 : @walk_left_right2
+    end
+
     if move_x > 0 then
+      @dir = :right
       move_x.times { @x += 1 }
     end
     if move_x < 0 then
+      @dir = :left
       (-move_x).times { @x -= 1 }
     end
 
     if move_y > 0 then
+      @dir = :right
       move_y.times { @y += 1 }
     end
     if move_y < 0 then
+      @dir = :left
       (-move_y).times { @y -= 1 }
     end
   end
