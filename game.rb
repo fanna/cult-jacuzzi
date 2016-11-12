b9215348d03aeea985387da532c9d4a4152ccbba
Stefan Mijucic <fanna993@gmail.com>
2016-11-12 12:45:23 +0100

    Integrate wikipedia

--- game.rb
+++ game.rb
@@ -1,15 +1,25 @@
 require 'gosu'
 require_relative './lib/player.rb'
+require_relative './lib/wikipedia.rb'
+
+WIDTH = 640
+HEIGHT = 640
 
 class GameWindow < Gosu::Window
   def initialize
-    super 640, 640
+    super WIDTH, HEIGHT
     self.caption = "Cult Jacuzzi"
     @background_image = Gosu::Image.new("./assets/floor.jpg", :tileable => true)
+    @text_image = Gosu::Image.new("./assets/black.jpg")
     @player = Player.new
 
     @pos_x = 100
     @pos_y = 100
+
+    @font = Gosu::Font.new(15)
+    @text = "TEST"
+
+    @text_background = false
   end
 
   def update
@@ -22,11 +32,20 @@ class GameWindow < Gosu::Window
   def draw
     @background_image.draw(0, 0, 0)
     @player.draw(@pos_x, @pos_y)
+
+    if @text_background == true
+      @text_image.draw(10, 10, 0)
+      @font.draw(@string, 20, 20, 3, 1.0, 1.0, 0xffff00ff)
+    end
   end
 
   def button_down(id)
     case id
-    when Gosu::Button::KbEscape then close
+    when Gosu::Button::KbEscape
+      close
+    when Gosu::Button::KbSpace
+      @text_background = true
+      @text = Wikipedia.random_text
     end
   end
 end
